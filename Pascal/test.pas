b88528979bbd4616a61186496de54cf7f882ffeb
hzfGuardian <412571747@qq.com>
2016-06-16 23:00:23 +0800

    merge test.pas

--- Pascal/test.pas
+++ Pascal/test.pas
@@ -1,16 +1,42 @@
-program yu;
-const aa = 5;
-type ii=integer;
-jj = array [5..10] of integer;
-var n,m :jj;
-x,y:ii; 
-begin
- m[5]:= 0;
- m[6]:= 1;
- m[7]:= 28;
- m[8]:= 3;
- m[9]:= 4;
- m[10]:= 7;
- x := m[m[10]]+aa;
- writeln(x); 
-end.
+Program HelloWorld;
+  
+  {const_part}
+  const 
+    MAX = 1000;
+      MIN = 100;
+
+  {type_part}
+  type
+    a = INTEGER; 
+    b = ARRAY [1..10] OF INTEGER; {array `simple_type_decl` of `type_decl` }
+    c = b;
+        d = (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec); 
+        e = MIN..MAX;
+ 
+    Date=Record                  
+      {field_decl_list}
+      {field_decl}
+                {name_list  :  type_decl  ;}
+          day, son: 1..31;
+          vas: INTEGER;
+          yea: 1970..1999;
+      End;
+    
+    {var_part}
+    var x, y, z: INTEGER;
+        u, v: CHAR;
+    
+    {routine_part}
+    {(function_decl or procedure_decl)*}
+    
+    function Double2(Value:Integer):Integer;
+    begin
+        Double2 := Value * 2;
+  end;
+ 
+    {routine_body}
+  begin
+        {stmt_list}
+        {stmt  ';'}
+      writeln('Hello, world!');
+  end.
